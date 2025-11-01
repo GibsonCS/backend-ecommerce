634a7f5c3abc248a2f083ca4bdf4f050a09442d0
GibsonCS <gibson.cruzds@gmail.com>
2025-11-01 00:54:16 -0300

    test: create test to create-user use case

--- test/create-user.test.ts
+++ test/create-user.test.ts
@@ -1,7 +1,92 @@
-import { describe, test } from "@jest/globals";
+import { beforeEach, describe, expect, it, vi } from "vitest";
+import CreateUser from "../src/application/use-cases/create-user.js";
+import User from "../src/core/entities/user.js";
+import SqliteUserRepositry from "../src/infrastructure/databases/sqlite/sqlite-user-repository.js";
 
 describe("create-user", () => {
-	test("should create a user and return user id", async () => {
-		console.log("ss");
+	let createUser: CreateUser;
+	beforeEach(() => {
+		createUser = new CreateUser(new SqliteUserRepositry());
+	});
+
+	it("should return an error if the login is invalid", async () => {
+		vi.spyOn(createUser, "execute").mockRejectedValue(
+			"Login must be three or more characters",
+		);
+
+		const input = {
+			login: "Gi",
+			password: "123456",
+			email: "gibson@asdgibson.com",
+		};
+
+		await expect(() => createUser.execute(input)).rejects.toThrow(
+			"Login must be three or more characters",
+		);
+	});
+
+	it("should return an error if the password is invalid", async () => {
+		vi.spyOn(createUser, "execute").mockRejectedValue(
+			"Password must be six or more characters",
+		);
+		const input = {
+			login: "Gibson",
+			password: "12",
+			email: "gibson@dcvgibson.com",
+		};
+
+		await expect(() => createUser.execute(input)).rejects.toThrow(
+			"Password must be six or more characters",
+		);
+	});
+
+	it("should return an error if the email is invalid", async () => {
+		vi.spyOn(createUser, "execute").mockRejectedValue("Email is invalid");
+		const input = {
+			login: "Gibson",
+			password: "1234567",
+			email: "gibson",
+		};
+
+		await expect(() => createUser.execute(input)).rejects.toThrow(
+			"Email is invalid",
+		);
+	});
+
+	it("should return an error if the user already exist", async () => {
+		const input = {
+			login: "Gibson",
+			password: "1234567",
+			email: "gibson@gibson.com",
+		};
+
+		vi.spyOn(
+			SqliteUserRepositry.prototype,
+			"findByEmail",
+		).mockResolvedValueOnce(new User(input));
+
+		await expect(() => createUser.execute(input)).rejects.toThrow(
+			"User already exists",
+		);
+	});
+
+	it("should create an user and return a object with {userId: UUID}", async () => {
+		vi.spyOn(SqliteUserRepositry.prototype, "findByEmail").mockResolvedValue(
+			null,
+		);
+
+		vi.spyOn(SqliteUserRepositry.prototype, "save").mockResolvedValue();
+
+		const input = {
+			login: "Gibson",
+			password: "1234567",
+			email: "gibson8@gibson.comsss",
+		};
+
+		const result = await createUser.execute(input);
+
+		expect(result).toMatchObject({
+			userId: expect.any(String),
+		});
 	});
 });
